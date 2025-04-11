b373955c168e2e146b53c5c86dddf357b6e3b490
adirifta <adiriftadk@gmail.com>
2025-04-11 16:03:12 +0700

    add: data kuliner

--- data/kuliner.js
+++ data/kuliner.js
@@ -1,3 +1,169 @@
-const kuliner = ["nanti di isi data kuliner"];
+const kuliner = [
+    {
+        "id": 1,
+        "name": "Momong Resto",
+        "description": "Momong Resto adalah restoran keluarga dengan konsep modern, yang ramah kantong dan ramah anak.",
+        "price": 21000,
+        "img": "https://lh3.googleusercontent.com/gps-cs-s/AB5caB_B67isRnDk8ToWu_dqhDwr3pf0rDCTy-3MigFxYpvVAhMLWy_YppbqdksMt3fZLV0rHBhVcJIYvQoLbgPKGJ4t5WNF9sCImuwWlckCaYH0EHPdA8Z6MZuktYEKIjzVSaGOdzTC=w408-h306-k-no",
+        "category": "Kedai Sup",
+        "location": "Jl. H.M. Sarbini No.142 B, Megabiru, Karangsari, Kec. Kebumen, Kabupaten Kebumen, Jawa Tengah 54317",
+        "contact": "081227404342",
+        "ratings": 4.3
+      },
+      {
+        "id": 2,
+        "name": "Warung Makan Rosya",
+        "description": "Makanan andalannya sop daging",
+        "price": 25000,
+        "img": "https://lh3.googleusercontent.com/gps-cs-s/AB5caB9LAhPCHjEoQG_-LyCy4-3a81kmhLhadNUeVjsGjXED62qHcnGxzfZQmslYUyL4x3MQ4c7ssf7ACF9mfryImxpiIViT0BINpZgIGclwX5ovshfeHv5SqleTT1qGohvMvOMG6numRA=w408-h306-k-no",
+        "category": "Kedai Sup",
+        "location": "Jl. Kutoarjo No.39, Pekisen, Selang, Kec. Kebumen, Kabupaten Kebumen, Jawa Tengah 54314",
+        "contact": "0287383284",
+        "ratings": 4.5
+      },
+      {
+        "id": 3,
+        "name": "Sate Ambal",
+        "description": "Sate ayam khas Ambal dengan bumbu kacang khas dan daging empuk",
+        "price": 15000,
+        "img": "https://www.finnafood.com/blog/wp-content/uploads/2024/09/sate-ambal.jpg",
+        "category": "Sate",
+        "location": "Jl. Raya Ambal, Kebumen",
+        "contact": "083234567892",
+        "ratings": 4.8
+      },
+      {
+        "id": 4,
+        "name": "Lanting Kering",
+        "description": "Kerupuk lanting khas Kebumen dengan rasa gurih dan renyah",
+        "price": 12000,
+        "img": "https://images.tokopedia.net/img/cache/500-square/product-1/2020/2/28/6549035/6549035_96d7b6f8-b2ff-4147-bad0-0d0f08bff35a_640_640.jpg",
+        "category": "Snack",
+        "location": "Ds. Tanjungsari, Kec. Kebumen",
+        "contact": "084334567893",
+        "ratings": 4.4
+      },
+      {
+        "id": 5,
+        "name": "Gethuk Goreng",
+        "description": "Olahan singkong goreng dengan taburan kelapa parut dan gula merah",
+        "price": 10000,
+        "img": "https://radarbanyumas.disway.id/upload/21f2411871cc16ea65d41cc9f6f10379.jpg",
+        "category": "Jajanan",
+        "location": "Pasar Wonosari, Kebumen",
+        "contact": "085434567894",
+        "ratings": 4.3
+      },
+      {
+        "id": 6,
+        "name": "Sroto Sokaraja Kebumen",
+        "description": "Soto khas dengan kuah kental dan tambahan lontong serta tauge",
+        "price": 18000,
+        "img": "https://i.gojekapi.com/darkroom/gofood-indonesia/v2/images/uploads/a3022ec4-8be1-4a54-8968-719b6e23c153_Go-Biz_20221111_152857.jpeg",
+        "category": "Soto",
+        "location": "Jl. Pemuda No. 45, Kebumen",
+        "contact": "086534567895",
+        "ratings": 4.6
+      },
+      {
+        "id": 7,
+        "name": "Nasi Penggel",
+        "description": "Nasi dengan lauk ayam kampung dan sambal khas, dibungkus daun pisang",
+        "price": 15000,
+        "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQw1p3lpO38qzDBzJ0L6p5Ld8WkMr-OJ3VdMg&s",
+        "category": "Makanan",
+        "location": "Jl. Veteran No. 12, Kebumen",
+        "contact": "087634567896",
+        "ratings": 4.5
+      },
+      {
+        "id": 8,
+        "name": "Klepon Ketawa",
+        "description": "Klepon khas Kebumen dengan isi gula merah yang melimpah",
+        "price": 8000,
+        "img": "https://statik.tempo.co/data/2017/04/24/id_601563/601563_650.jpg",
+        "category": "Jajanan",
+        "location": "Pasar Selang, Kebumen",
+        "contact": "088734567897",
+        "ratings": 4.2
+      },
+      {
+        "id": 9,
+        "name": "Wedang Uwuh",
+        "description": "Minuman herbal khas Kebumen dengan campuran rempah-rempah",
+        "price": 5000,
+        "img": "https://res.cloudinary.com/dk0z4ums3/image/upload/v1725439821/attached_image/7-manfaat-wedang-uwuh-bagi-kesehatan-dan-cara-membuatnya.jpg",
+        "category": "Minuman",
+        "location": "Jl. Patimura No. 34, Kebumen",
+        "contact": "089834567898",
+        "ratings": 4.3
+      },
+      {
+        "id": 10,
+        "name": "Keripik Gadung",
+        "description": "Keripik umbi gadung dengan rasa gurih dan renyah",
+        "price": 15000,
+        "img": "https://down-id.img.susercontent.com/file/sg-11134201-23010-d279qd25a6lv3b",
+        "category": "Snack",
+        "location": "Ds. Karangsari, Kebumen",
+        "contact": "081934567899",
+        "ratings": 4.1
+      },
+      {
+        "id": 11,
+        "name": "Tahu Bacem Kebumen",
+        "description": "Tahu bacem dengan bumbu khas yang meresap sempurna",
+        "price": 7000,
+        "img": "https://img-global.cpcdn.com/recipes/c497732071ecf327/680x482cq70/tahu-bacem-santen-foto-resep-utama.jpg",
+        "category": "Makanan",
+        "location": "Jl. Ahmad Yani No. 56, Kebumen",
+        "contact": "082044567800",
+        "ratings": 4.4
+      },
+      {
+        "id": 12,
+        "name": "Dawet Ireng",
+        "description": "Es dawet dengan cendol hitam dari bahan alami dan kuah santan gula merah",
+        "price": 8000,
+        "img": "https://awsimages.detik.net.id/content/2015/11/13/297/budayaindonesia.org.jpg",
+        "category": "Minuman",
+        "location": "Alun-alun Kebumen",
+        "contact": "082144567811",
+        "ratings": 4.7
+      },
+      {
+        "id": 13,
+        "name": "Opor Panggang",
+        "description": "Opor ayam dengan bumbu khas yang dipanggang hingga harum",
+        "price": 20000,
+        "img": "https://img-global.cpcdn.com/recipes/bf73e7e16cb0dd06/680x482cq70/opor-ayam-panggang-jepara-foto-resep-utama.jpg",
+        "category": "Makanan",
+        "location": "Jl. Diponegoro No. 22, Kebumen",
+        "contact": "082244567822",
+        "ratings": 4.6
+      },
+      {
+        "id": 14,
+        "name": "Krupuk Rambak",
+        "description": "Kerupuk kulit sapi atau kerbau dengan tekstur renyah",
+        "price": 18000,
+        "img": "https://img-global.cpcdn.com/recipes/bf73e7e16cb0dd06/680x482cq70/opor-ayam-panggang-jepara-foto-resep-utama.jpg",
+        "category": "Snack",
+        "location": "Ds. Tanjungmeru, Kebumen",
+        "contact": "082344567833",
+        "ratings": 4.3
+      },
+      {
+        "id": 15,
+        "name": "Sego Megono",
+        "description": "Nasi dengan lauk megono (olahan nangka muda) khas Kebumen",
+        "price": 12000,
+        "img": "https://img-global.cpcdn.com/recipes/f674005faf99972c/680x482cq70/sego-megono-khas-purworejo-foto-resep-utama.jpg",
+        "category": "Makanan",
+        "location": "Jl. Pahlawan No. 78, Kebumen",
+        "contact": "082444567844",
+        "ratings": 4.5
+      }
+];
 
 module.exports = kuliner;
